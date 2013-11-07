4930e1a8e13eed5596340534efd9e3546975dbb6
Lee Crossley <leee@hotmail.co.uk>
2013-11-07 08:53:27 +0000

    Grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,32 @@
 module.exports = function (grunt) {
-    grunt.initConfig();
+    grunt.initConfig({
+        jshint: {
+            all: [
+                "*.js",
+                "<%= nodeunit.tests %>"
+            ]
+        },
+        clean: {
+            output: ["specs/output/*"]
+        },
+        pogo: {
+            compile: {
+                files: {
+                    "specs/output/pogo-sample.js": ["specs/pogo-sample.pogo"]
+                }
+            }
+        },
+        nodeunit: {
+            tests: ["specs/*spec.js"]
+        }
+    });
+
+    grunt.loadTasks("tasks");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-nodeunit");
+    grunt.loadNpmTasks("grunt-contrib-internal");
+
+    grunt.registerTask("test", ["clean", "jshint", "pogo", "nodeunit"]);
+    grunt.registerTask("default", ["test", "build-contrib"]);
 };
