df17c7675d5574796b1ab3a42c962ddebfa79e9e
Lee Crossley <leee@hotmail.co.uk>
2015-02-08 10:54:53 +0000

    Remove internal

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,8 +26,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-nodeunit");
-    grunt.loadNpmTasks("grunt-contrib-internal");
 
     grunt.registerTask("test", ["clean", "jshint", "pogo", "nodeunit"]);
-    grunt.registerTask("default", ["test", "build-contrib"]);
+    grunt.registerTask("default", ["test"]);
 };
